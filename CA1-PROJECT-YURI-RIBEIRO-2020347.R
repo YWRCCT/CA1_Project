eb6378bc3f77dd7dcb30a204f3dfa676f0a76bc0
YWRCCT <145453082+YWRCCT@users.noreply.github.com>
2023-12-02 16:36:16 +0000

    updated code heatmap

--- CA1-PROJECT-YURI-RIBEIRO-2020347.R
+++ CA1-PROJECT-YURI-RIBEIRO-2020347.R
@@ -24,6 +24,11 @@ print(numerical_summary_skim)
 
 
 # ------------------ Preparation of the data ---------------------------
+library(stringi)
+# 
+crimes_complete$OFFENSE_DESCRIPTION <- stri_trans_general(crimes_complete$OFFENSE_DESCRIPTION, "Latin-ASCII")
+crimes_complete$DAY_OF_WEEK <- stri_trans_general(crimes_complete$DAY_OF_WEEK, "Latin-ASCII")
+
 
 # Replace 'Y' with 'YES' and null with 'NO' in the 'SHOOTING' column
 crimes_complete <- crimes_complete %>%
@@ -66,4 +71,42 @@ crimes_complete_zScore <- crimes_complete %>%
   mutate(across(all_of(columns_to_apply), scale))
 print(select(crimes_complete_zScore, all_of(columns_to_apply)))    
 
+
+# Heatmap 
+# First we create a data frame to combine the two variables ( Offense Description and Days of Week) selecting 5 offenses
+
+selected_offenses <- c("BURGLARY - RESIDENTIAL - NO FORCE", "HARASSMENT", "WARRANT ARREST", "VERBAL DISPUTE", "VANDALISM")
+
+
+# create the data frame for the heatmap
+heatmap_dataFrame <- crimes_complete %>%
+  filter(OFFENSE_DESCRIPTION %in% selected_offenses) %>%
+  group_by(OFFENSE_DESCRIPTION, DAY_OF_WEEK) %>%
+  summarise(count = n_distinct(INCIDENT_NUMBER)) %>%
+  ungroup()
+
+# Create a heatmap using ggplot2
+heatmap_plot <- ggplot(heatmap_dataFrame, aes(x = DAY_OF_WEEK, y = OFFENSE_DESCRIPTION, fill = count)) +
+  geom_tile() +
+  scale_fill_gradient(low = "lightblue", high = "red") +
+  labs(title = "Heatmap of Selected Offenses vs. Days of Week",
+       x = "Days of Week",
+       y = "Offense Description") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# Display the heatmap
+plot(heatmap_plot)
+
+
+
+#  Dummy coding to the categorical OFFENSE_DESCRIPTION column
+dummy_coding <- model.matrix(~ OFFENSE_DESCRIPTION - 1, data = crimes_complete)
+
+# Combine the dummy-coded variables with the original data
+crimes_complete_dummy <- cbind(crimes_complete, dummy_coding)
+
+# View the resulting data
+
+crimes_complete_dumy
 
