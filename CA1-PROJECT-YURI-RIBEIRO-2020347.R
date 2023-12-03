6575d29e24b0580dcee44475d84fdf231dbb62e2
YWRCCT <145453082+YWRCCT@users.noreply.github.com>
2023-12-03 16:09:50 +0000

    code updated - exploration - bar plots

--- CA1-PROJECT-YURI-RIBEIRO-2020347.R
+++ CA1-PROJECT-YURI-RIBEIRO-2020347.R
@@ -1,3 +1,10 @@
+library(dplyr)
+library(gridExtra)
+library(stringi)
+library(skimr)
+library(ggplot2)
+
+
 # Read the dataset 
 crimes_Boston <- read.csv("crime.csv")
 offense_codes <- read.csv("offense_codes.csv")
@@ -6,7 +13,7 @@ offense_codes <- read.csv("offense_codes.csv")
 print(head(crimes_Boston))
 print(head(offense_codes))
 
-library(dplyr)
+
 
 # merge both datasets and drop the column CODE from offense_codes
 crimes_complete <- left_join(crimes_Boston, offense_codes %>% select(CODE, NAME), by = c("OFFENSE_CODE" = "CODE"))
@@ -15,7 +22,7 @@ head(crimes_complete)
 
 # With the library skimr we can have a complete data summary with the mean/median, minimum, maximum and standard deviation
 install.packages("skimr")
-library(skimr)
+
 numerical_summary_skim <- skim(crimes_complete)
 
 print(numerical_summary_skim)
@@ -24,7 +31,7 @@ print(numerical_summary_skim)
 
 
 # ------------------ Preparation of the data ---------------------------
-library(stringi)
+
 # 
 crimes_complete$OFFENSE_DESCRIPTION <- stri_trans_general(crimes_complete$OFFENSE_DESCRIPTION, "Latin-ASCII")
 crimes_complete$DAY_OF_WEEK <- stri_trans_general(crimes_complete$DAY_OF_WEEK, "Latin-ASCII")
@@ -47,7 +54,7 @@ cat("Missing values (%):\n")
 print(missing_percentage)
 
 # Data type of variables and bar plot
-library(ggplot2)
+
 var_types <- sapply(crimes_complete, class)
 plot_data_types <- data.frame(variable = names(var_types), type = var_types)
 
@@ -108,5 +115,80 @@ crimes_complete_dummy <- cbind(crimes_complete, dummy_coding)
 
 # View the resulting data
 
-crimes_complete_dumy
+print(crimes_complete_dummy)
+
+
+# Applying PCA to all numeric variables
+
+# replace missing values with mean 
+crimes_complete_v2 <- na.omit(crimes_complete)
+
+# Extract the numeric columns for PCA
+numeric_columns <- sapply(crimes_complete_v2, is.numeric)
+data_for_pca <- crimes_complete_v2[, numeric_columns]
+standardized_data <- scale(data_for_pca)
+
+
+# PCA RESULT
+pca_result <- prcomp(standardized_data, scale. = TRUE)
+
+# Summary of PCA
+summary(pca_result)
+
+# PCA plot
+plot(pca_result, type = "l", main = "PCA Plot")
+
+
+# EXPLORATION
+
+# Create a horizontal bar plot - Which district has the most/least number of reported crimes?
+
+ggplot(crimes_complete, aes(x = reorder(DISTRICT, -table(DISTRICT)[DISTRICT]), fill = DISTRICT)) +
+  geom_bar(stat = "count") +
+  labs(x = "Number of Reported Crimes", y = "District") +
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 8)) +  
+  coord_flip() 
+
+
+# Create a vertical bar plot - which hours has more number of reported crimes?
+bar_plot_hour <- ggplot(crimes_complete, aes(x = HOUR)) +
+  geom_bar(stat = "count", fill = "skyblue") +
+  labs(x = "Hour of the Day", y = "Number of Reported Crimes") +
+  theme_minimal() +
+  scale_x_continuous(breaks = seq(0, 23, 1), labels = sprintf("%02d:00", seq(0, 23, 1)))
+
+# Display the bar plot
+print(bar_plot_hour)
+
+
+# The Top 10 and bottom of Crimes:
+#Create a temporary data frame of top 10
+top10_crimes <- data.frame(
+  OFFENSE_CODE_GROUP = names(table(crimes_complete$OFFENSE_CODE_GROUP)),
+  Count_crimes = as.numeric(table(crimes_complete$OFFENSE_CODE_GROUP)))
+
+# Order the data frame by Count_crimes
+top10_crimes <- top10_crimes[order(-top10_crimes$Count_crimes), ]
+
+# Top 10 Types of Crimes 
+top_10_plot_horizontal <- ggplot(top10_crimes[1:10, ], 
+                                 aes(x = Count_crimes, y = reorder(OFFENSE_CODE_GROUP, -Count_crimes))) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "Number of Reported Crimes", y = "") +
+  ggtitle("Top 10 Types of Crimes") +
+  theme_minimal() +
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5))  
+
+# Bottom 10 Types of Crimes 
+bottom_10_plot_horizontal <- ggplot(top10_crimes[(nrow(top10_crimes)-9):nrow(top10_crimes), ], 
+                                    aes(x = Count_crimes, y = reorder(OFFENSE_CODE_GROUP, Count_crimes))) +
+  geom_bar(stat = "identity", fill = "salmon") +
+  labs(x = "Number of Reported Crimes", y = "") +
+  ggtitle("Bottom 10 Types of Crimes") +
+  theme_minimal() +
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5))  
+
+# display the plots 
+grid.arrange(top_10_plot_horizontal, bottom_10_plot_horizontal, ncol = 1)
 
